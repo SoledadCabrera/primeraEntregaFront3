36c4ecf46956b053fb7d1ff5be2bb085b221abbf
solecabrera <soledadcabrera123@gmail.com>
2022-03-29 20:43:22 -0300

    subo entregable

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Padre from "./components/Padre";
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Padre />
+      </div>
+    );
+  }
+  
+}
+
+export default App;
